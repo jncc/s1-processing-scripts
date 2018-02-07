716b8e8edfa06cd948728d4e72447ebfcc65e9d7
felix <felix.mason@jncc.gov.uk>
2018-02-07 14:21:20 +0000

    Snap toolbox v 2.2

--- JNCC_S1_GRD_configfile_v.1.0.sh
+++ JNCC_S1_GRD_configfile_v.1.0.sh
@@ -3,19 +3,19 @@
 ##### Directories set up  
 ##### ==================
 ##### INPUT directories
-export MAIN_DIR="{{ s1_ard_main_dir }}" ##== MAIN DIRECTORY with DEM, Input dataset and Outputs
-export BASKET_INDIR="{{ s1_ard_basket_dir }}" ##== directory containing  S1_GRDH.zip products to be processed
+export MAIN_DIR="/home/aminchella/JNCC/MAIN" ##== MAIN DIRECTORY with DEM, Input dataset and Outputs  
+export BASKET_INDIR="${MAIN_DIR}/Basket" ##== directory containing  S1_GRDH.zip products to be processed
 #####
-export EXTDEMFILE="{{ s1_ard_ext_dem }}" ##== External APGB DEM
+export EXTDEMFILE="${MAIN_DIR}/EXT_DEM/APGB_Qgis.tif" ##== External APGB DEM
 export EXTDEMNOVAL="-32768.0" ## External DEM No data value
 ##### OUTPUT directories
 export MAIN_OUTDIR="${MAIN_DIR}/OUTPUT"  ##== MAIN OUTPUT DIRECTORY where products output folders will be created 
 ##### After Processing
 export PROZIP_DIR="${MAIN_DIR}/ZIP_PROCESSED"  ##== directory where S1.zip data are moved after processing
 ##### SW and processing xml chains directories
-export GRAPHSDIR="{{ s1_ard_script_dir }}/xml" ##== DIRECTORY with snap xml graphs for the processing 
-export SNAP_HOME="{{ snap_bin_path }}" ##== SNAP (version 5) directory
-export SCRIPT_DIR="{{ s1_ard_script_dir }}"
+export GRAPHSDIR="/home/aminchella/JNCC/script/xml" ##== DIRECTORY with snap xml graphs for the processing 
+export SNAP_HOME="/opt/snap/bin" ##== SNAP (version 5) directory
+export SCRIPT_DIR="/home/aminchella/JNCC/script"
 #####============================================================
 ##### STATIC Variable for Log files   
 #####============================================================
